a1ba1f0fb61d8dde04da6f6d7b70a70d29a1a174
Harry Perez <14997@holbertonstudents.com>
2026-07-20 10:50:03 -0400

    Print numbers using putchar

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+/**
+ * main - Prints all single digit numbers using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	number = 0;
+	while (number < 10)
+	{
+		putchar(number + '0');
+		number++;
+	}
+	putchar('\n');
+
+	return (0);
+}
