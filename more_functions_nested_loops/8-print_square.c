31da27b128ce63ae68c861236205ebb57728c59b
Harry Perez <14997@holbertonstudents.com>
2026-07-20 13:15:11 -0400

    Add print square function

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,32 @@
+#include "main.h"
 
+/**
+ * print_square - Prints a square using hash characters
+ * @size: Size of the square
+ *
+ * Return: Nothing
+ */
+void print_square(int size)
+{
+	int row;
+	int column;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	row = 0;
+	while (row < size)
+	{
+		column = 0;
+		while (column < size)
+		{
+			_putchar('#');
+			column++;
+		}
+		_putchar('\n');
+		row++;
+	}
+}
