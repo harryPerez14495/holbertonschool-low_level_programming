fe79b20aa8b132f704a47ca490e99423b5f3f89b
Harry Perez <14997@holbertonstudents.com>
2026-07-20 10:46:37 -0400

    Print single digit numbers

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+/**
+ * main - Prints all single digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	number = 0;
+	while (number < 10)
+	{
+		printf("%d", number);
+		number++;
+	}
+	printf("\n");
+
+	return (0);
+}
