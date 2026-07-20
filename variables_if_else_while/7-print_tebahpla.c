60ba7265ed4e25cfd87ccbb236788e78ccc17314
Harry Perez <14997@holbertonstudents.com>
2026-07-20 10:53:05 -0400

    Print alphabet in reverse

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+/**
+ * main - Prints the lowercase alphabet in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+
+	return (0);
+}
