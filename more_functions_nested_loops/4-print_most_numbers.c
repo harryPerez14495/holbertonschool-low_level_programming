b46f8da647f887fd1e35036f6f1a158a35e397aa
Harry Perez <14997@holbertonstudents.com>
2026-07-20 12:36:31 -0400

    Add print most numbers function

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+ * print_most_numbers - Prints numbers from 0 to 9 except 2 and 4
+ *
+ * Return: Nothing
+ */
+void print_most_numbers(void)
+{
+	int number;
+
+	number = 0;
+	while (number <= 9)
+	{
+		if (number != 2 && number != 4)
+			_putchar(number + '0');
+		number++;
+	}
+	_putchar('\n');
+}
