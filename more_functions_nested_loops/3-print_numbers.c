0f030ee157909dc88f36c31f906a71df14780111
Harry Perez <14997@holbertonstudents.com>
2026-07-20 12:23:06 -0400

    Add print numbers function

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,19 @@
+#include "main.h"
 
+/**
+ * print_numbers - Prints the numbers from 0 to 9
+ *
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	int number;
+
+	number = 0;
+	while (number <= 9)
+	{
+		_putchar(number + '0');
+		number++;
+	}
+	_putchar('\n');
+}
