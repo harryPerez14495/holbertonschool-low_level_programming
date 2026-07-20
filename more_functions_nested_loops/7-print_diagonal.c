59726d63aeaf5b410bafbab2b3435245e59bebb1
Harry Perez <14997@holbertonstudents.com>
2026-07-20 13:04:01 -0400

    Add print diagonal function

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,33 @@
+#include "main.h"
 
+/**
+ * print_diagonal - Draws a diagonal line using backslashes
+ * @n: Number of backslashes to print
+ *
+ * Return: Nothing
+ */
+void print_diagonal(int n)
+{
+	int line;
+	int spaces;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	line = 0;
+	while (line < n)
+	{
+		spaces = 0;
+		while (spaces < line)
+		{
+			_putchar(' ');
+			spaces++;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		line++;
+	}
+}
