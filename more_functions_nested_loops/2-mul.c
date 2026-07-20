41c332708bf618511bf59a2ebbea1aa7d75d0cdd
Harry Perez <14997@holbertonstudents.com>
2026-07-20 12:08:03 -0400

    Fix mul function

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -5,4 +5,17 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 
-#endif
+#endi
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: The result of a multiplied by b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}f
