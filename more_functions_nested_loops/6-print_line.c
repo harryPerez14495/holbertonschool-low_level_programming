3655a3f07be1366c0b9a344d67bf9a2a05e0f386
Harry Perez <14997@holbertonstudents.com>
2026-07-20 12:54:29 -0400

    Add print line function

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+ * print_line - Draws a straight line using underscores
+ * @n: Number of underscores to print
+ *
+ * Return: Nothing
+ */
+void print_line(int n)
+{
+	int count;
+
+	count = 0;
+	while (count < n)
+	{
+		_putchar('_');
+		count++;
+	}
+	_putchar('\n');
+}
