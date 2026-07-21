2eb6079c4ffef7b38debf79df2c1cf8c974d8f13
Harry Perez <14997@holbertonstudents.com>
2026-07-21 02:06:05 -0400

    Add string comparison function

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+ * _strcmp - Compares two strings
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: Difference between the first different characters
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int index;
+
+	index = 0;
+
+	while (s1[index] == s2[index] && s1[index] != '\0')
+		index++;
+
+	return ((unsigned char)s1[index] - (unsigned char)s2[index]);
+}
