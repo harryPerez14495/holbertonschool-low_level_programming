a075e79520bb25a8c10a738a65f1aa3d3343a73b
Harry Perez <14997@holbertonstudents.com>
2026-07-21 02:01:48 -0400

    Add limited string copy function

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,30 @@
+#include "main.h"
 
+/**
+ * _strncpy - Copies a string using at most n bytes
+ * @dest: Destination buffer
+ * @src: Source string
+ * @n: Number of bytes to copy
+ *
+ * Return: Pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index;
+
+	index = 0;
+
+	while (index < n && src[index] != '\0')
+	{
+		dest[index] = src[index];
+		index++;
+	}
+
+	while (index < n)
+	{
+		dest[index] = '\0';
+		index++;
+	}
+
+	return (dest);
+}
