d2c212f1f1d34e57de559ec921d86d6dc727ac80
Harry Perez <14997@holbertonstudents.com>
2026-07-21 01:52:29 -0400

    Add string concatenation function

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,31 @@
+#include "main.h"
 
+/**
+ * _strcat - Concatenates two strings
+ * @dest: Destination string
+ * @src: Source string
+ *
+ * Return: Pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dest_index;
+	int src_index;
+
+	dest_index = 0;
+	src_index = 0;
+
+	while (dest[dest_index] != '\0')
+		dest_index++;
+
+	while (src[src_index] != '\0')
+	{
+		dest[dest_index] = src[src_index];
+		dest_index++;
+		src_index++;
+	}
+
+	dest[dest_index] = '\0';
+
+	return (dest);
+}
