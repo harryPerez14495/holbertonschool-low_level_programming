83debaba2a42e3e2c1d927a3874ac0cacb047c75
Harry Perez <14997@holbertonstudents.com>
2026-07-20 23:20:30 -0400

    Add print alphabet ten times function

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,18 @@
+#include "main.h"
 
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int line;
+	char letter;
+
+	for (line = 0; line < 10; line++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+		_putchar('\n');
+	}
+}
