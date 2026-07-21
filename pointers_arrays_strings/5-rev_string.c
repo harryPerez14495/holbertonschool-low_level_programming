8a944c0c5021151b2cf3f640741a925e60ad5385
Harry Perez <14997@holbertonstudents.com>
2026-07-21 01:03:09 -0400

    Add string reversal function

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,29 @@
+#include "main.h"
 
+/**
+ * rev_string - Reverses a string
+ * @s: String to reverse
+ */
+void rev_string(char *s)
+{
+	int left;
+	int right;
+	char temporary;
+
+	right = 0;
+
+	while (s[right] != '\0')
+		right++;
+
+	right--;
+	left = 0;
+
+	while (left < right)
+	{
+		temporary = s[left];
+		s[left] = s[right];
+		s[right] = temporary;
+		left++;
+		right--;
+	}
+}
