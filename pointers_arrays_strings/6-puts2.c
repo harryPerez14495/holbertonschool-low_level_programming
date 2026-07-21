e8acf25f4c2b7dfd26331559fb2754b8f6b7bff0
Harry Perez <14997@holbertonstudents.com>
2026-07-21 01:15:39 -0400

    Fix puts2 for odd length strings

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,14 +7,15 @@
 void puts2(char *str)
 {
 	int index;
+	int length;
 
-	index = 0;
+	length = 0;
 
-	while (str[index] != '\0')
-	{
+	while (str[length] != '\0')
+		length++;
+
+	for (index = 0; index < length; index += 2)
 		_putchar(str[index]);
-		index += 2;
-	}
 
 	_putchar('\n');
 }
