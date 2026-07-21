d67f0f2f1492cf4546d5aeb81b1d0a625c16668e
Harry Perez <14997@holbertonstudents.com>
2026-07-21 01:33:03 -0400

    Add string copy function

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,25 @@
+#include "main.h"
 
+/**
+ * _strcpy - Copies a string to another buffer
+ * @dest: Destination buffer
+ * @src: Source string
+ *
+ * Return: Pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index;
+
+	index = 0;
+
+	while (src[index] != '\0')
+	{
+		dest[index] = src[index];
+		index++;
+	}
+
+	dest[index] = '\0';
+
+	return (dest);
+}
