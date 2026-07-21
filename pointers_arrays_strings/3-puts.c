71e7ce1b7b6d42a9a78c82e5a913e67b11c5ca1b
Harry Perez <14997@holbertonstudents.com>
2026-07-21 00:50:41 -0400

    Add puts function

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+ * _puts - Prints a string followed by a new line
+ * @str: String to print
+ */
+void _puts(char *str)
+{
+	int index;
+
+	index = 0;
+
+	while (str[index] != '\0')
+	{
+		_putchar(str[index]);
+		index++;
+	}
+
+	_putchar('\n');
+}
