aeb149aa1ed28e0c5d6f87eb5581bc371fa88c85
Harry Perez <14997@holbertonstudents.com>
2026-07-21 01:12:46 -0400

    Add every other character function

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+ * puts2 - Prints every other character of a string
+ * @str: String to print
+ */
+void puts2(char *str)
+{
+	int index;
+
+	index = 0;
+
+	while (str[index] != '\0')
+	{
+		_putchar(str[index]);
+		index += 2;
+	}
+
+	_putchar('\n');
+}
