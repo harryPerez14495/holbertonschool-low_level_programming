4b1e17c41a628db20590972641fd5ab6858bfb3b
Harry Perez <14997@holbertonstudents.com>
2026-07-21 02:15:28 -0400

    Add uppercase string function

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,24 @@
+#include "main.h"
 
+/**
+ * string_toupper - Changes lowercase letters to uppercase
+ * @s: String to modify
+ *
+ * Return: Pointer to s
+ */
+char *string_toupper(char *s)
+{
+	int index;
+
+	index = 0;
+
+	while (s[index] != '\0')
+	{
+		if (s[index] >= 'a' && s[index] <= 'z')
+			s[index] = s[index] - 'a' + 'A';
+
+		index++;
+	}
+
+	return (s);
+}
