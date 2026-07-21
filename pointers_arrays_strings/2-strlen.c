ffe05ae20d17b0385a7b2df83bc5ce661cfbef87
Harry Perez <14997@holbertonstudents.com>
2026-07-21 00:45:42 -0400

    Add string length function

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,19 @@
+#include "main.h"
 
+/**
+ * _strlen - Returns the length of a string
+ * @s: String to measure
+ *
+ * Return: Length of the string
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+
+	while (s[length] != '\0')
+		length++;
+
+	return (length);
+}
