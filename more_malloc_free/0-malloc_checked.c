5b81a2904d990249bead9cbe1dc7118e5ebc5f80
Harry Perez <14997@holbertonstudents.com>
2026-08-03 14:57:59 -0400

    Add checked memory allocation function

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: Pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *pointer;
+
+	pointer = malloc(b);
+
+	if (pointer == NULL)
+		exit(98);
+
+	return (pointer);
+}
---
+++ more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This directory contains C programs that use malloc, free, and exit.
