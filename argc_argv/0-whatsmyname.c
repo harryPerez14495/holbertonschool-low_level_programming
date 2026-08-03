ba9b39857b2da21ae1b7970436d6c3beb6ef9d76
Harry Perez <14997@holbertonstudents.com>
2026-08-03 14:26:29 -0400

    Add program that prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of command line arguments
+ * @argv: array containing the command line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory contains C programs that use command line arguments.
