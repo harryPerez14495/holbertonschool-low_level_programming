ace45ca332ae9f5ba54bf879be37bdfc17776709
Harry Perez <14997@holbertonstudents.com>
2026-08-03 14:34:08 -0400

    Add program that prints all arguments

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all command line arguments
+ * @argc: number of command line arguments
+ * @argv: array containing the command line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
