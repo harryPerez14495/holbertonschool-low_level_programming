baa754b1c0f11fe8908e153071ef7157e4edcf17
Harry Perez <14997@holbertonstudents.com>
2026-08-03 14:10:40 -0400

    Add function that allocates a grid

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a two dimensional array of integers
+ * @width: number of columns
+ * @height: number of rows
+ *
+ * Return: Pointer to the grid, or NULL if allocation fails
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int row;
+	int column;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(sizeof(int *) * height);
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (row = 0; row < height; row++)
+	{
+		grid[row] = malloc(sizeof(int) * width);
+
+		if (grid[row] == NULL)
+		{
+			while (row > 0)
+			{
+				row--;
+				free(grid[row]);
+			}
+
+			free(grid);
+			return (NULL);
+		}
+
+		for (column = 0; column < width; column++)
+			grid[row][column] = 0;
+	}
+
+	return (grid);
+}
