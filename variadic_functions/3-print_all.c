0a7a1a5da0e1bb3f8440f577eaf7b8301687acb8
Harry Perez <14997@holbertonstudents.com>
2026-08-11 20:06:17 -0400

    Fix Betty function length

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,15 +2,75 @@
 #include "variadic_functions.h"
 
 /**
+ * struct print_type - associates a type with a print function
+ * @type: format character
+ * @print: function used to print the argument
+ */
+typedef struct print_type
+{
+	char *type;
+	void (*print)(va_list arguments);
+} print_t;
+
+/**
+ * print_character - prints a character
+ * @arguments: list of arguments
+ */
+static void print_character(va_list arguments)
+{
+	printf("%c", va_arg(arguments, int));
+}
+
+/**
+ * print_integer - prints an integer
+ * @arguments: list of arguments
+ */
+static void print_integer(va_list arguments)
+{
+	printf("%d", va_arg(arguments, int));
+}
+
+/**
+ * print_float - prints a floating-point number
+ * @arguments: list of arguments
+ */
+static void print_float(va_list arguments)
+{
+	printf("%f", va_arg(arguments, double));
+}
+
+/**
+ * print_string - prints a string
+ * @arguments: list of arguments
+ */
+static void print_string(va_list arguments)
+{
+	char *string;
+
+	string = va_arg(arguments, char *);
+	if (string == NULL)
+		string = "(nil)";
+
+	printf("%s", string);
+}
+
+/**
  * print_all - prints arguments according to a format string
  * @format: list of argument types
  */
 void print_all(const char * const format, ...)
 {
+	print_t types[] = {
+		{"c", print_character},
+		{"i", print_integer},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
 	va_list arguments;
 	unsigned int i;
+	unsigned int j;
 	char *separator;
-	char *string;
 
 	i = 0;
 	separator = "";
@@ -18,31 +78,17 @@ void print_all(const char * const format, ...)
 
 	while (format != NULL && format[i] != '\0')
 	{
-		switch (format[i])
+		j = 0;
+		while (types[j].type != NULL)
 		{
-		case 'c':
-			printf("%s%c", separator, va_arg(arguments, int));
-			separator = ", ";
-			break;
-		case 'i':
-			printf("%s%d", separator, va_arg(arguments, int));
-			separator = ", ";
-			break;
-		case 'f':
-			printf("%s%f", separator, va_arg(arguments, double));
-			separator = ", ";
-			break;
-		case 's':
-			string = va_arg(arguments, char *);
-			if (string == NULL)
-				string = "(nil)";
-			printf("%s%s", separator, string);
-			separator = ", ";
-			break;
-		default:
-			break;
+			if (format[i] == types[j].type[0])
+			{
+				printf("%s", separator);
+				types[j].print(arguments);
+				separator = ", ";
+			}
+			j++;
 		}
-
 		i++;
 	}
 
