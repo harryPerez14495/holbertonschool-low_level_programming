ace6b1554e3d73b1c806b417960343b857c8b0a8
Harry Perez <harryPerez14495@users.noreply.github.com>
2026-08-28 10:14:07 -0400

    Fix Betty style for cp program

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -17,7 +17,49 @@ void close_descriptor(int fd)
 }
 
 /**
- * main - copies the content of one file into another file
+ * open_source - opens the source file
+ * @filename: name of the source file
+ *
+ * Return: source file descriptor
+ */
+int open_source(const char *filename)
+{
+	int fd;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	return (fd);
+}
+
+/**
+ * open_destination - opens or creates the destination file
+ * @filename: name of the destination file
+ * @fd_from: source file descriptor
+ *
+ * Return: destination file descriptor
+ */
+int open_destination(const char *filename, int fd_from)
+{
+	int fd;
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd == -1)
+	{
+		close_descriptor(fd_from);
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", filename);
+		exit(99);
+	}
+	return (fd);
+}
+
+/**
+ * main - copies the content of one file into another
  * @argc: number of command-line arguments
  * @argv: array containing the command-line arguments
  *
@@ -25,10 +67,8 @@ void close_descriptor(int fd)
  */
 int main(int argc, char *argv[])
 {
-	int fd_from;
-	int fd_to;
-	ssize_t bytes_read;
-	ssize_t bytes_written;
+	int fd_from, fd_to;
+	ssize_t bytes_read, bytes_written;
 	char buffer[1024];
 
 	if (argc != 3)
@@ -36,23 +76,8 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		close_descriptor(fd_from);
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+	fd_from = open_source(argv[1]);
+	fd_to = open_destination(argv[2], fd_from);
 
 	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
 	{
@@ -64,16 +89,13 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-
 	if (bytes_read == -1)
 	{
 		dprintf(STDERR_FILENO,
 			"Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	close_descriptor(fd_from);
 	close_descriptor(fd_to);
-
 	return (0);
 }
